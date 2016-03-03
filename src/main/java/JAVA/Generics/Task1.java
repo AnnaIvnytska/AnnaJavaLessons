681539a9221a2e3c362dbd4ebe2e284fef1ebb49
Anna Ivnytska <anna.ivnytska@gmail.com>
2016-03-03 13:26:28 +0200

    AnnaUpdate

--- src/main/java/JAVA/Generics/Task1.java
+++ src/main/java/JAVA/Generics/Task1.java
@@ -16,31 +16,44 @@ import java.util.Map;
 
 public class Task1 {
 
-    public static Map<String, Double> totalizer(Map<String, List<? super Number>> map) {
-        Double summOfValues = (double) 0;
+    public static Map<String, Double> totalizer(Map<String, List<? extends Number>> map) {
         Map<String, Double> newReturnMap = new HashMap<>();
 
-        for (Map.Entry<String, List<? super Number>> entry : map.entrySet()) {
+        for (Map.Entry<String, List<? extends Number>> entry : map.entrySet()) {
+            Double a = (double) 0;
             for (int i = 0; i < entry.getValue().size(); i++) {
-                Double a = (double) (entry.getValue().get(i));
-                summOfValues += a;
+                a += entry.getValue().get(i).doubleValue();
             }
-
-            newReturnMap.put(entry.getKey(), summOfValues);
+            newReturnMap.put(entry.getKey(), a);
         }
         return newReturnMap;
     }
 
     public static void main(String[] args) {
         Task1 task1 = new Task1();
-        List<? super Number> l = new ArrayList<>();
-        l.add(1);
-        l.add(1.1);
-        l.add(888888888);
 
-        Map<String, List<? super Number>> list = new HashMap<>();
-        list.put("first", l);
-        totalizer(list);
+        List<Double> valuePart1 = new ArrayList<Double>();
+        valuePart1.add(1.1);
+        valuePart1.add(1.1);
+        valuePart1.add(1.1);
+
+        List<Integer> valuePart2 = new ArrayList<Integer>();
+        valuePart2.add(1);
+        valuePart2.add(1);
+        valuePart2.add(1);
+
+        List<Long> valuePart3 = new ArrayList<Long>();
+        valuePart3.add(2147483648L);
+        valuePart3.add(2147483648L);
+        valuePart3.add(2147483648L);
+
+        Map<String, List<? extends Number>> mapList = new HashMap<>();
+        mapList.put("first", valuePart1);
+        mapList.put("second", valuePart2);
+        mapList.put("third", valuePart3);
+        Map<String, Double> result = totalizer(mapList);
+        System.out.println(mapList.entrySet());
+        System.out.println(result.entrySet());
     }
 }
 
